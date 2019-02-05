6b90b1b87598786242f3c7c35196221320c4671f
MarcoQin <qyyfy2009@gmail.com>
2019-02-05 10:29:14 +0800

    add sub command

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ extern crate lava_rs;
 extern crate lazy_static;
 extern crate rustyline;
 
-use clap::{App, Arg};
+use clap::{App, Arg, SubCommand};
 use lava_rs::player;
-use rustyline::Editor;
 use rustyline::error::ReadlineError;
-use std::sync::Arc;
+use rustyline::Editor;
+use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
+use std::sync::Arc;
 use std::thread;
 use std::time;
 
@@ -23,36 +24,57 @@ fn main() {
         .version("0.1")
         .author("Marco Qin")
         .about("Nothing but just a player")
-        .arg(Arg::with_name("INPUT")
-            .help("Sets the input music file to play")
-            .required(true)
-            .index(1))
-        .arg(Arg::with_name("c")
-            .short("c")
-            .long("control")
-            .multiple(false)
-            .help("Set whether need to control player by command"))
+        .arg(
+            Arg::with_name("INPUT")
+                .help("Sets the input music file to play")
+                .required(true)
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("c")
+                .short("c")
+                .long("control")
+                .multiple(false)
+                .help("Set whether need to control player by command"),
+        )
         .get_matches();
     let file_name = matches.value_of("INPUT").unwrap();
     println!("Get input file: {}", file_name);
 
+    let command_parser = App::new("command")
+        .version(".01")
+        .subcommand(
+            SubCommand::with_name("add").arg(Arg::with_name("path").required(true).index(1)),
+        )
+        .subcommand(
+            SubCommand::with_name("volume").arg(Arg::with_name("volume").required(true).index(1)),
+        );
+
     let use_control;
 
     match matches.occurrences_of("c") {
-        0 => { use_control = false; }
-        1 => { use_control = true; }
-        _ => { use_control = false; }
+        0 => {
+            use_control = false;
+        }
+        1 => {
+            use_control = true;
+        }
+        _ => {
+            use_control = false;
+        }
     }
 
     player::init_player();
     player::load_file(file_name.to_string());
+    let mut cmd: String = "".to_string();
 
     if !use_control {
         let running = Arc::new(AtomicBool::new(true));
         let r = running.clone();
         ctrlc::set_handler(move || {
             r.store(false, Ordering::SeqCst);
-        }).expect("Error setting Ctrl-C handler");
+        })
+        .expect("Error setting Ctrl-C handler");
         while running.load(Ordering::SeqCst) {
             if player::is_stopping() {
                 break;
@@ -76,8 +98,51 @@ fn main() {
                 Ok(line) => {
                     println!("get command: {}", line);
                     rl.add_history_entry(line.as_ref());
+                    let line = line.trim().replace("\\", "");
                     if line == "q" {
                         break;
+                    }
+                    let mut arg_vet = vec!["command"];
+                    cmd.clone_from(&line);
+                    if let Some(idx) = cmd.find(" ") {
+                        let c = cmd.split_at(idx);
+                        arg_vet.push(c.0);
+                        arg_vet.push(c.1.trim());
+                        match command_parser.clone().get_matches_from_safe(arg_vet) {
+                            Ok(sub_commands) => {
+                                if let Some(sub_m) = sub_commands.subcommand_matches("add") {
+                                    if sub_m.is_present("path") {
+                                        let path = sub_m.value_of("path").unwrap();
+                                        let real_path = Path::new(path);
+                                        if real_path.exists() && real_path.is_file() {
+                                            player::load_file(path.to_string());
+                                        }
+                                    }
+                                } else if let Some(sub_m) =
+                                    sub_commands.subcommand_matches("volume")
+                                {
+                                    if sub_m.is_present("volume") {
+                                        let volume = sub_m.value_of("volume").unwrap();
+                                        match volume.parse::<i32>() {
+                                            Ok(real_volume) => {
+                                                let real_volume = if real_volume > 100 {
+                                                    100
+                                                } else {
+                                                    real_volume
+                                                };
+                                                let real_volume =
+                                                    if real_volume < 0 { 0 } else { real_volume };
+                                                player::set_volume(real_volume);
+                                            }
+                                            Err(_) => {}
+                                        }
+                                    }
+                                }
+                            }
+                            Err(err) => {
+                                println!("error {:?}", err);
+                            }
+                        }
                     }
                 }
                 Err(ReadlineError::Interrupted) => {
